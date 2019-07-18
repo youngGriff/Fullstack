a9fe1a81590083bdce91adc6cf831eb443b09140
Andriy <andriy.gaidar99@gmail.com>
2019-07-18 22:48:24 +0300

    init

--- app.js
+++ app.js
@@ -31,10 +31,13 @@ app.use('/api/order', orderRoutes);
 app.use('/api/position', positionRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/dist/client'));
-    app.get('*', (req, res)=>{
+    app.use(express.static('client/dist/client'))
+
+    app.get('*', (req, res) => {
         res.sendFile(
-            path.resolve(__dirname, 'client', 'dist', 'client', 'index.html')
+            path.resolve(
+                __dirname, 'client', 'dist', 'client', 'index.html'
+            )
         )
     })
 }
